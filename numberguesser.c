89649dc3f92ce7910bbf514805027c6dd3c98abd
jelwingeuan <jelwingeuanjw@gmail.com>
2024-06-29 15:55:50 +0800

    improve1

--- numberguesser.c
+++ numberguesser.c
@@ -3,20 +3,30 @@
 #include <time.h>
 
 int main() {
-    int number, guess, nguesses = 1;
+    int number, guess, nguesses = 0;
     srand(time(0));
     number = rand() % 100 + 1; 
+
+    printf("Welcome to the Number Guesser Game!\n");
+    printf("I have selected a number between 1 and 100.\n");
+
     do {
-        printf("Guess the number between 1 to 100\n");
-        scanf("%d", &guess);
-        if (guess > number) {
-            printf("Lower number please!\n");
-        } else if (guess < number) {
-            printf("Higher number please!\n");
-        } else {
-            printf("You guessed it in %d attempts\n", nguesses);
+        printf("Enter your guess: ");
+        while (scanf("%d", &guess) != 1 || guess < 1 || guess > 100) {
+            printf("Invalid input. Please enter a number between 1 and 100: ");
+            while (getchar() != '\n'); 
         }
         nguesses++;
+
+        if (guess > number) {
+            printf("Too high! Try a lower number.\n");
+        } else if (guess < number) {
+            printf("Too low! Try a higher number.\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", nguesses);
+        }
     } while (guess != number);
+
+    printf("Thank you for playing!\n");
     return 0;
-} 
+}
